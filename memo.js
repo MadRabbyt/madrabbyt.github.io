e81ac43ec6e059bf32ab727b4d785ab50be5baa0
Mad <madrabbyt@gmail.com>
2019-11-27 23:21:31 -0500

    Update memo.js - random pairs
    Add algorithm for distribute pairs among table cells

--- memo.js
+++ memo.js
@@ -5,12 +5,27 @@ let memoBoard = document.getElementById("memo-board");
 let n = 4;
 let m = n * n;
 let p = m / 2;
+let inrow = [];
+let nums = [];
+for (let i = 0; i < m; i += 1) {inrow.push(0); nums.push(i);}
+let pair1 = 0;
+for (let i = 0; i < p; i += 1) {
+  pair1 = Math.floor(Math.random()*nums.length);
+  inrow[nums[pair1]] = i;
+  nums.splice(pair1, 1);
+  pair1 = Math.floor(Math.random()*nums.length);
+  inrow[nums[pair1]] = i;
+  nums.splice(pair1, 1);
+}
 let game = [];
 let row = [];
+pair1 = 0;
 for(let i = 0; i < n; i += 1) {
   row = [];
   for(let j = 0; j < n; j += 1) {
-    row.push(Math.floor(Math.random()*m));
+//    row.push(Math.floor(Math.random()*m));
+    row.push(inrow[pair1]);
+    pair1 += 1;
   }
   game.push(row);
 }
